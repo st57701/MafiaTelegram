8a73ebc80e48d8d8682301671c5c0a6c3c4e44c1
st57701 <st57701@gmail.com>
2020-09-06 22:14:07 +0300

    ADDED CONFIG TOKEN AND ADDED CHAT ID

---
+++ config.py
@@ -0,0 +1,2 @@
+TOKEN = '1171053168:AAH_gE4r8bZLbSNmqDZY7eWsNW7JX9XtBZs'  # Learn how to get one on https://core.telegram.org/bots#6-botfather
+PLAYERS_COUNT_TO_START = 4
---
+++ src/bot.py
@@ -0,0 +1,11 @@
+import config as conf
+from telebot import TeleBot
+
+print(conf.TOKEN)
+
+tb= TeleBot(conf.TOKEN)
+#tb.send_message('-492482025', 'privet')
+#TeleBot.send_message(conf.TOKEN,'-492482025', 'privet')
+-1001403548604
+tb.send_message(-1001403548604, 'privet')
+tb.send_message(-412189564, 'poka')
